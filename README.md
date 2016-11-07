0cfb68ef7e6877de8a6b119d6baa1264beb54f7f
Miriam Hernandez <miriamhz14@gmail.com>
2016-11-07 17:46:56 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # ProjectSimone
+### Link:
+https://miriamhz14.github.io/DemoDeploy/
+
+### Planning Phase:
+- create a child's game using some of Simon's rules
+- create a random function for the pattern that would display 4 numbers and then increse as the player does better
+- create function to record what button is being pressed and that it matches the pattern
+- have a way to let user know that the button was clicked (color change, sound, opacity change, etc..)
+- create a function to keep score and determine when the player has won the game.
+- animation for a win situation.
+- used HTML, CSS, JavaScript and JQuery on this project
+
+
+### How it works:
+- the game starts with a random pattern displayed on the botttom which has four numbers.
+- user must click on the pattern correctly to move on. (works only with mouse)
+- if user passes the first round the pattern will increase by one more number.
+- if user fails to follow the pattern they will fail and have a chance to try again.
+- there is no timer as this is a child game.
+
+
+### Purpose of game 
+- a fun way for kids to learn
+- education game for preschoolers to start recognizing numbers
+- help kids become used to computers and how it works
+
+
+###User Story
+- as a teacher/ user  I want to be able to use this game online. Have a fun way for the kids to learn their numbers in small increments
+   have ways they can interact with the game (have them know when they won and when they fail as welll as have control to continue and 
+   replay if needed)
--- script.js
+++ script.js
@@ -5,11 +5,17 @@ $(document).ready(function(){
   var buttons = $('.btn');
   var cubes =[ 1,2,3,4];
   var counter = 0;
-  var user = [ ];
-  var startGame = $('#start');
+  var cont = $('#cont');
   var newPat = $('#pat');
   var arrRnd = [];
+  var test = [];
+  var score = 0;
+  var scoreDisplay = $('#score');
+  var status = $('#status');
+  var restart = $('#try');
 
+
+  // use to keep function clean no math needed inside the function
   function rndRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
@@ -18,79 +24,74 @@ $(document).ready(function(){
     $('.btn:nth-child(' + i +')').append(i);
   }
 
+
+
+  var tracker = [];
+  var count = 0;
+
+
+// keeps tracks of what user has clicked and lets them know if they are right or wrong.
+// keeps track of the score once a pattern has been completed they recieve one point
+// amination for you won
   buttons.click(function(event) {
-    console.log("hello")
-    $(this).toggleClass('active');
-    for(var i=0; i < arrRnd.length; i++) {
 
+  tracker.push(Number($(this).text()));
+  for (var i = 0; i < tracker.length; i++) {
+    if ((tracker.toString() === arrRnd.toString()) && (tracker.length === arrRnd.length)){
+      status.text('Status: Good Job!!!!');
+      score++;
+      scoreDisplay.text('Score: ' + score);
+      tracker = [];
+      if (score === 1){
+        status.text('YOU WON!!!!!!!!!');
+        status.animate({'font-size':'40px'},100);
+        status.animate({'margin-left':'40px'},100);
+        status.animate({'margin-left':'0px'},100);
+        status.animate({'margin-left':'40px'},100);
+        status.animate({'margin-left':'0px'},100);
+      }
     }
-    //if()
-  });
-
-
-  // buttons.click(function(event) {
-  //   this.style.backgroundColor = 'purple';
-  // });
-
-  startGame.click(function(event){
-  while (arrRnd.length < 4){
-    var rndnumber = rndRange(1,4);
-    if(arrRnd.indexOf(rndnumber) > 1) continue;
-    arrRnd[arrRnd.length] = rndnumber;
-    newPat.append(rndnumber);
+    if ((tracker.toString() !== arrRnd.toString()) && (tracker.length === arrRnd.length)){
+      status.text('Status: Try Again');
+      restart.css('display','inline-block');
+    }
   }
+    console.log(tracker);
+    // goes pack to when the game first started  (restart of game)
+  restart.click(function(event){
+    location.reload();
+  })
+});
+
+
+  // used to create a random pattern that spits out four numbers and after that adds one more
+    var pattern = function(num){
+         for (var i = 0; i<num; i++){
+          arrRnd.push(rndRange(1,4));
+        }
+        $('#pat').text('Pattern: ' + arrRnd);
+    }
+  cont.click(function(event){
+    if (counter === 0){
+      pattern(4);
+      counter++;
+    }
+    else {
+      pattern(1);
+      counter++;
+    }
+      console.log(arrRnd);
  });
-console.log();
 
-
-  //  startGame.click(function(event) {
-  //   var rndnumber = rndRange(1,4);
-  //   for (var i = 1; i <cubes.length; i++);
-  //   newPat.append(rndnumber);
-  // });
 
 });
 
-// var selectPat = function(){
-//   var rndPat = rnd(0,4)
-//   $('footer').text('here we go! ')
-//   $('#key').text(' the sequence is' + rndPat )
-// };
-
-// console.log(selectPat())
-//  });
 
 
 
 
 
-// change the order of buttons once the player  has passed a level
-// add a timer
-// increase the pattern by 1
-// createtwo functions and call the second function inside the 1st one
-// make the game more challenging.
 
 
-// var cube = document.querySelectorAll ('.zone');
-// var counter = 0
 
 
-// for (var i= 0; i <cube.length; i++){
-//     cube[i].onmouseover = function() {
-//     this.className += ' green';
-//     };
-
-//   cube[i].onmouseout = function() {
-//     this.className = ' zone';
-//      };
-
-//   cube[i].onclick = function() {
-//     this.style.backgroundColor = ' green';
-//     // counter++ counts the zones that have already been clicked
-//     counter++;
-
-//     if( counter == cube.length){
-//       console.log("Congradulations!")
-//         };
-//     }
-// };
